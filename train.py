b18088c973c1796429bf032e7d8dc8ab8f516320
jhampier <jhampiertapia@gmail.com>
2025-10-31 23:57:55 -0500

    MLOPS-JTS

--- train.py
+++ train.py
@@ -52,7 +52,7 @@ def train_and_eval():
     X_train, X_test, y_train, y_test = train_test_split(
       X, y, test_size=0.2, random_state=42
     )
-    clf = LogisticRegression(max_iter=1000, multi_class="auto", random_state=8888)fit(X_train, y_train)
+    clf = LogisticRegression(max_iter=1000, multi_class="auto", random_state=8888).fit(X_train, y_train)
     # pipe = Pipeline(steps=[
     #     ("scaler", StandardScaler()),
     #     ("clf", LogisticRegression(max_iter=1000, multi_class="auto", random_state=8888))
