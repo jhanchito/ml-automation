cbdc10ba615f3d797bc446cbaee96f31118cf7c0
jhampier <jhampiertapia@gmail.com>
2025-10-31 23:56:16 -0500

    MLOPS-JTS

--- train.py
+++ train.py
@@ -52,38 +52,38 @@ def train_and_eval():
     X_train, X_test, y_train, y_test = train_test_split(
       X, y, test_size=0.2, random_state=42
     )
-    clf = LogisticRegression(max_iter=1000, multi_class="auto", random_state=8888)
+    clf = LogisticRegression(max_iter=1000, multi_class="auto", random_state=8888)fit(X_train, y_train)
     # pipe = Pipeline(steps=[
     #     ("scaler", StandardScaler()),
     #     ("clf", LogisticRegression(max_iter=1000, multi_class="auto", random_state=8888))
     # ])
-    yhat = clf.predict(X_test)
+    y_pred = clf.predict(X_test)
     metrics = {
         "accuracy": float(accuracy_score(y_test, y_train)),
         "f1_macro": float(f1_score(y_test, y_train, average="macro")),
         "classes": df["Churn"].unique().tolist()
     }
-    bundle = {"model": clf, "target_names": iris.target_names.tolist()}
+    bundle = {"model": clf, "target_names": df["Churn"].unique().tolist()}
     save_model_bundle(bundle)
     print("Train OK:", metrics)
     return metrics
 
 
-    pipe.fit(X_train, y_train)
+    # pipe.fit(X_train, y_train)
 
-    # Evaluación
-    y_pred = pipe.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print(f"Accuracy: {acc:.4f}")
-    print(classification_report(y_test, y_pred))
+    # # Evaluación
+    # y_pred = pipe.predict(X_test)
+    # acc = accuracy_score(y_test, y_pred)
+    # print(f"Accuracy: {acc:.4f}")
+    # print(classification_report(y_test, y_pred))
 
-    Path(os.path.dirname(model_path)).mkdir(parents=True, exist_ok=True)
-    joblib.dump({
-        "pipeline": pipe,
-        "target_names": df["Churn"].unique(),
-        "feature_names": numeric_features
-    }, model_path)
-    print(f"Modelo guardado en: {model_path}")
+    # Path(os.path.dirname(model_path)).mkdir(parents=True, exist_ok=True)
+    # joblib.dump({
+    #     "pipeline": pipe,
+    #     "target_names": df["Churn"].unique(),
+    #     "feature_names": numeric_features
+    # }, model_path)
+    # print(f"Modelo guardado en: {model_path}")
 
 if __name__ == "__main__":
     train_and_eval()
